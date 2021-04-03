9a041311f1183e0994888959089b0450ff255d60
Alexey-Gaidai <71748686+Alexey-Gaidai@users.noreply.github.com>
2021-04-03 20:47:53 +0700

    чета получается

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Laba04._04
 {
@@ -6,7 +9,26 @@ namespace Laba04._04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, string> abc = new Dictionary<string, string>();
+            abc.Add("ghbdtn", "привет");
+            abc.Add("rfr ltkf&", "как дела?");
+            abc.Add("првиет", "привет");
+
+            string str = string.Empty;
+            using (StreamReader reader = File.OpenText(@"D:\test.txt"))
+            {
+                str = reader.ReadToEnd();
+            }
+            foreach (KeyValuePair<string, string> keyValue in abc)
+            {
+                str = str.Replace(keyValue.Key, keyValue.Value);
+                Console.WriteLine(keyValue.Key + " - " + keyValue.Value);
+            }
+
+            using (StreamWriter file = new StreamWriter(@"D:\test.txt"))
+            {
+                file.Write(str);
+            }
         }
     }
 }

f686834b3b672315417950bd5fc6ccb2c8dcc455
Alexey-Gaidai <71748686+Alexey-Gaidai@users.noreply.github.com>
2021-04-04 13:35:35 +0700

    Энд

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Laba04._04
 {
@@ -11,71 +11,33 @@ namespace Laba04._04
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> abc = new Dictionary<string, string>();
+            Dictionary<string, string> abc = new Dictionary<string, string>();//создаем словарь ошибочных и правильных слов
             abc.Add("ghbdtn", "привет");
             abc.Add("rfr ltkf&", "как дела?");
             abc.Add("првиет", "привет");
 
+            string path = Console.ReadLine();//вводим путь в директорию где нужно выполнить работу
+            string str = string.Empty;//очищаем строку в которой будут проводиться действия
+            string target = "+380 12";//значение на которое будем менять номера
 
-            string path = Console.ReadLine();
-            string str = string.Empty;
-            /*using (StreamReader reader = File.OpenText(@"D:\test.txt"))
+            var dirs = Directory.GetFiles(path, "*", SearchOption.AllDirectories);//создаем переменную в которой хранятся пути к файлам в директории
+            foreach (var files in dirs)//для каждого файла в директории выполняем следующее:
             {
-                str = reader.ReadToEnd();
-            }
-            foreach (KeyValuePair<string, string> keyValue in abc)
-            {
-                str = str.Replace(keyValue.Key, keyValue.Value);
-                Console.WriteLine(keyValue.Key + " - " + keyValue.Value);
-            }
-
-            using (StreamWriter file = new StreamWriter(@"D:\test.txt"))
-            {
-                file.Write(str);
-            }*/
-            
-            var keywords = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
-            foreach (var files in keywords)
-            {
-                Console.WriteLine(files);
-                using (StreamReader reader = File.OpenText(files))
+                Console.WriteLine(files);//выводим файлы 
+                using (StreamReader reader = File.OpenText(files))//открываем текст
                 {
-                    str = reader.ReadToEnd();
+                    str = reader.ReadToEnd();//читаем до конца
                 }
-                foreach (KeyValuePair<string, string> keyValue in abc)
+                foreach (KeyValuePair<string, string> keyValue in abc)//для каждого ключазначения в словаре абс выполняем:
                 {
-                    str = str.Replace(keyValue.Key, keyValue.Value);
-                    Console.WriteLine(keyValue.Key + " - " + keyValue.Value);
+                    str = str.Replace(keyValue.Key, keyValue.Value);//заменяем найденные "неправильные" ключи "правильным" значениями
+                    Regex regex = new Regex(@"\w*.012.\w*");//создаем регулярное выражение которое ищет все что содержит 012 вместе с рядом стоящими занчениями(у нас это "()")
+                    str = regex.Replace(str, target);//выполняем замену в снашем тексте на +380 12
                 }
 
-                using (StreamWriter file = new StreamWriter(files))
-                {
+                using (StreamWriter file = new StreamWriter(files))//записываем в файл
                     file.Write(str);
-                }
-
             }
         }
     }
-    /*public class search
-    {
-        public string directory;
-        public string word;
-        public search(string keyword, string path)
-        {
-            this.directory = path;
-            this.word = keyword;
-
-        }
-
-        public void result()
-        {
-            var keywords = from search in Directory.GetFiles(directory, "*", SearchOption.AllDirectories) where File.ReadAllLines(search).Contains(word) select search;
-
-            foreach (var files in keywords)
-            {
-                Console.WriteLine(files);
-
-            }
-        }
-    }*/
 }
